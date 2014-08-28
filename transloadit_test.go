46e04d2128a2892e25e27314f08461a60ecb6f76
Acconut <maerious@gmail.com>
2014-08-28 15:22:20 +0200

    Dropped current go-sdk

--- transloadit_test.go
+++ transloadit_test.go
@@ -1,36 +1,3 @@
 package transloadit
 
 import "testing"
-
-var (
-	apikey = "b3a87350e97a11e380176daba9afca9d"
-	secret = "91e76a7b699e5590268917c4bbdc65141471e40b"
-)
-
-func TestRequest(t *testing.T) {
-	i, err := NewInstance(apikey, secret)
-	if err != nil {
-		t.Fatal(err)
-	}
-	//fmt.Print(i)
-	p := Params{
-		Auth: Auth{
-			Key: i.apikey,
-		},
-		Steps: map[string]interface{}{
-			"resize": map[string]interface{}{
-				"robot":  "/image/resize",
-				"width":  200,
-				"height": 200,
-				"use":    ":original",
-			},
-		},
-	}
-	p.Init()
-	//fmt.Print(p)
-	filepath := "/Users/slowpoke/Pictures/test/aface.png"
-	_, err = i.CreateAssembly(p, filepath)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
