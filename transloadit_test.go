4f692adcbb3ceda3cb23860d4a0bc61a6367b85e
Acconut <maerious@gmail.com>
2014-08-29 14:43:05 +0200

    Added tests for creating client

--- transloadit_test.go
+++ transloadit_test.go
@@ -1,1 +1,42 @@
 package transloadit
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCreateClient(t *testing.T) {
+
+	client, err := NewClient(&DefaultConfig)
+	if client != nil {
+		t.Fatal("client should be nil")
+	}
+
+	if !strings.Contains(err.Error(), "missing AuthKey") {
+		t.Fatal("error should contain message")
+	}
+
+	config := DefaultConfig
+	config.AuthKey = "fooo"
+	client, err = NewClient(&config)
+	if client != nil {
+		t.Fatal("client should be nil")
+	}
+
+	if !strings.Contains(err.Error(), "missing AuthSecret") {
+		t.Fatal("error should contain message")
+	}
+
+	config = DefaultConfig
+	config.AuthKey = "fooo"
+	config.AuthSecret = "bar"
+	client, err = NewClient(&config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if client == nil {
+		t.Fatal("client should not be nil")
+	}
+
+}
