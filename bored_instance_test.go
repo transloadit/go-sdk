5ab066e735f85a48dc691ed5d0e21095f86f02e8
Acconut <maerious@gmail.com>
2014-08-31 23:05:48 +0200

    Added getBoredInstance fallback Closes #3

--- bored_instance_test.go
+++ bored_instance_test.go
@@ -18,3 +18,24 @@ func TestGetBoredInstance(t *testing.T) {
 	}
 }
 
+func TestGetBoredInstanceFallback(t *testing.T) {
+
+	config := DefaultConfig
+	config.AuthKey = "auth_key"
+	config.AuthSecret = "auth_secret"
+	config.Endpoint = "http://doesnotexists/"
+
+	client, err := NewClient(&config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bored, err := client.getBoredInstance()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if bored == "" {
+		t.Fatal("no bored instance provided")
+	}
+}
