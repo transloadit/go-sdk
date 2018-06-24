95291331f536a1958256c910122ea3c4c47edd89
Acconut <maerious@gmail.com>
2018-06-24 14:15:40 +0200

    Accept I/O timeouts as errors after cancelling

--- wait_test.go
+++ wait_test.go
@@ -43,15 +43,20 @@ func TestWaitForAssembly_Cancel(t *testing.T) {
 	t.Parallel()
 	client := setup(t)
 
-	ctx, cancel := context.WithTimeout(ctx, time.Millisecond)
+	ctx, cancel := context.WithTimeout(ctx, 100*time.Nanosecond)
 	defer cancel()
 
 	_, err := client.WaitForAssembly(ctx, &AssemblyInfo{
 		AssemblySSLURL: "https://api2.transloadit.com/assemblies/foo",
 	})
+
 	// Go 1.8 and Go 1.7 have different error messages if a request get canceled.
 	// Therefore we test for both cases.
-	if !strings.Contains(err.Error(), "context deadline exceeded") && !strings.Contains(err.Error(), "request canceled") {
+	// Sometimes, a "dial tcp: i/o timeout" error is thrown if the context times
+	// out shortly before the dialing is started, see:
+	// https://sourcegraph.com/github.com/golang/go@d6a27e8edcd992b36446c5021a3c7560d983e9a6/-/blob/src/net/dial.go#L123-125
+	// Therefore we also accept i/o timeouts as errors here.
+	if !strings.Contains(err.Error(), "context deadline exceeded") && !strings.Contains(err.Error(), "request canceled") && !strings.Contains(err.Error(), "i/o timeout") {
 		t.Fatalf("operation's deadline should be exceeded: %s", err)
 	}
 }
