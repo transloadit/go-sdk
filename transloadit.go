1b18056341121b968a049ed280b85ca55a638fc5
CARRIERE Etienne <etienne.a.carriere@socgen.com>
2022-08-08 12:10:44 +0200

    Add a random element in the payload to prevent Signature reuse (#35)
    * Add a random element in the payload to prevent Signature reuse
    
    * Update transloadit.go
    
    Co-authored-by: Marius <marius@transloadit.com>

--- transloadit.go
+++ transloadit.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"strings"
@@ -98,7 +99,9 @@ func (client *Client) sign(params map[string]interface{}) (string, string, error
 		Key:     client.config.AuthKey,
 		Expires: getExpireString(),
 	}
-
+	// Add a random nonce to make signatures unique and prevent error about
+	// signature reuse: https://github.com/transloadit/go-sdk/pull/35
+	params["nonce"] = rand.Int()
 	b, err := json.Marshal(params)
 	if err != nil {
 		return "", "", fmt.Errorf("unable to create signature: %s", err)
